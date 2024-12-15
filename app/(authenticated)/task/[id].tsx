9b94030b8b9ec8ce497e5ef867630f2d2d8824b9
Syket Bhattachergee <syketweb@gmail.com>
2024-12-15 23:47:58 +0600

    task details page added

--- app/(authenticated)/task/[id].tsx
+++ app/(authenticated)/task/[id].tsx
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
-const TaskDetails = () => {
-  return (
-    <View>
-      <Text>TaskDetails</Text>
-    </View>
+import TodoForm from "@/components/TodoForm";
+import { projects, todos } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite";
+import { useLocalSearchParams } from "expo-router";
+import { useSQLiteContext } from "expo-sqlite";
+
+const Page = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db);
+  const { data } = useLiveQuery(
+    drizzleDb
+      .select()
+      .from(todos)
+      .where(eq(todos.id, Number(id)))
+      .leftJoin(projects, eq(todos.project_id, projects.id))
   );
+
+  if (!data || data.length === 0) {
+    return null;
+  }
+
+  const todo = {
+    ...data?.[0].todos,
+    project_name: data?.[0].projects?.name || "",
+    project_color: data?.[0].projects?.color || "",
+  };
+
+  return <TodoForm todo={todo} />;
 };
-export default TaskDetails;
-const styles = StyleSheet.create({});
+export default Page;
